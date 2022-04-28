677d6916570c6c3deb317ea17eefdc874c3c7c2e
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-04-28 11:28:58 +0200

    last update

--- sensors_deviceMotion.js
+++ sensors_deviceMotion.js
@@ -1,9 +1,23 @@
 function displayError(msg) {
     document.getElementById("error").innerHTML += msg + "<br>";
 }
+
+function getOrientation(compas){
+    switch(Math.ceil(compas)){
+        case x < 45 || x >= 315: 
+            return "N";
+        case x < 135:
+            return "E";
+        case x < 225:
+            return "S";
+        case x < 315:
+            return "W";
+
+    }
+}
 window.addEventListener('deviceorientation', function(compas) {
     console.log(compas.alpha + ' : ' + compas.beta + ' : ' + compas.gamma);
-    this.document.getElementById("gyro").innerHTML = `${Math.ceil(compas.alpha)} : ${Math.ceil(compas.beta)} : ${Math.ceil(compas.gamma)}`
+    this.document.getElementById("gyro").innerHTML = `Magnetic Declination: X-Orientation: ${getOrientation(compas.alpha)} <br> Y = ${Math.ceil(compas.beta)} <br> Z = ${Math.ceil(compas.gamma)}`
   });
 
 window.addEventListener('devicemotion', function(event) {
