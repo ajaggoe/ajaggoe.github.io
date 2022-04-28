2799e1d4e65d9250394c314f56fec0e2108c20ef
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-04-28 11:17:19 +0200

    last update

--- sensors_deviceMotion.js
+++ sensors_deviceMotion.js
@@ -3,10 +3,10 @@ function displayError(msg) {
 }
 window.addEventListener('deviceorientation', function(compas) {
     console.log(compas.alpha + ' : ' + compas.beta + ' : ' + compas.gamma);
-    this.document.getElementById("gyro").innerHTML = `${compas.alpha} : ${compas.beta} : ${compas.gamma}`
+    this.document.getElementById("gyro").innerHTML = `${Math.ceil(compas.alpha)} : ${Math.ceil(compas.beta)} : ${Math.ceil(compas.gamma)}`
   });
 
 window.addEventListener('devicemotion', function(event) {
     console.log(event.acceleration.x + ' m/s2');
-    this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x} <br>y: ${event.acceleration.y}`
+    this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x.toFixed(2)} <br>y: ${event.acceleration.y.toFixed(2)}`
 });
