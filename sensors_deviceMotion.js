b99ffdb19cd3269e18d22cb91f695dd0d61344a4
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-04-28 11:39:07 +0200

    last update

--- sensors_deviceMotion.js
+++ sensors_deviceMotion.js
@@ -24,3 +24,25 @@ window.addEventListener('devicemotion', function(event) {
     console.log(event.acceleration.x + ' m/s2');
     this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x.toFixed(2)} <br>y: ${event.acceleration.y.toFixed(2)}`
 });
+
+navigator.permissions.query({ name: 'ambient-light-sensor' }).then(result => {
+    if (result.state === 'denied') {
+        console.log('Permission to use ambient light sensor is denied.');
+        return;
+    }
+
+    const als = new AmbientLightSensor({frequency: 20});
+    als.addEventListener('activate', () => console.log('Ready to measure EV.'));
+    als.addEventListener('error', event => console.log(`Error: ${event.error.name}`));
+    als.addEventListener('reading', () => {
+        // Defaut ISO value.
+        const ISO = 100;
+        // Incident-light calibration constant.
+        const C = 250;
+
+        let EV = Math.round(Math.log2((als.illuminance * ISO) / C));
+        console.log(`Exposure Value (EV) is: ${EV}`);
+    });
+
+    als.start();
+});
