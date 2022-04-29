23fb5e5df94fed5cd17844cf2bb683fde1beffdc
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-04-29 15:00:09 +0200

    check if there

--- sensors_deviceMotion.js
+++ sensors_deviceMotion.js
@@ -20,17 +20,22 @@ function getOrientation(compas){
 
 }
 
-window.addEventListener('deviceorientation', function(compas) {
+if(typeof(DeviceOrientationEvent) !== "undefined"){
+  window.addEventListener('deviceorientation', function(compas) {
     console.log(compas.alpha + ' : ' + compas.beta + ' : ' + compas.gamma);
     this.document.getElementById("gyro").innerHTML = `Magnetic Declination: X-Orientation: ${getOrientation(compas.alpha)} <br> Y = ${Math.ceil(compas.beta)} <br> Z = ${Math.ceil(compas.gamma)}`
     });
+}
 
-window.addEventListener('devicemotion', function(event) {
-    console.log(event.acceleration.x + ' m/s2');
-    this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x.toFixed(2)} <br>y: ${event.acceleration.y.toFixed(2)}`
-    this.document.getElementById("gyro").style.left = event.x * 3 + 20
 
-});
+if(typeof( DeviceMotionEvent ) !== "undefined" ){
+  window.addEventListener('devicemotion', function(event) {
+      console.log(event.acceleration.x + ' m/s2');
+      this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x.toFixed(2)} <br>y: ${event.acceleration.y.toFixed(2)}`
+      this.document.getElementById("gyro").style.left = event.x * 3 + 20
+
+  });
+}
 
 /**
  * FLASHLIGHT
