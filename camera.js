c90d8d812e83df9ca77631f56f856ac1a48066fb
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-05-04 09:29:41 +0200

    uh

--- camera.js
+++ camera.js
@@ -8,7 +8,7 @@ const startDrawing = () => {
     const metadataInfo =  document.querySelector("#metadata-info");
     
     button.addEventListener('click', () => video.paused ? video.play() : video.pause());
-  
+
     video.addEventListener('play', () => {
       if (!('requestVideoFrameCallback' in HTMLVideoElement.prototype)) {
         return alert('Your browser does not support the `Video.requestVideoFrameCallback()` API.');
@@ -21,33 +21,40 @@ const startDrawing = () => {
     let paintCount = 0;
     let startTime = 0.0;
   
-    // const updateCanvas = (now, metadata) => {
-    //   if (startTime === 0.0) {
-    //     startTime = now;
-    //   }
+    const updateCanvas = (now, metadata) => {
+      if (startTime === 0.0) {
+        startTime = now;
+      }
   
-    //   ctx.drawImage(video, 0, 0, width, height);
+      ctx.drawImage(video, 0, 0, width, height);
   
-    //   const elapsed = (now - startTime) / 1000.0;
-    //   const fps = (++paintCount / elapsed).toFixed(3);
-    //   fpsInfo.innerText = !isFinite(fps) ? 0 : fps;
-    //   metadataInfo.innerText = JSON.stringify(metadata, null, 2);
-  
-    //   video.requestVideoFrameCallback(updateCanvas);
-    // };  
-    let screenshot = function () {
-        console.log("bla")
-        ctx.drawImage(video, 0, 0, width, height);
-        var data = canvas.toDataURL('image/jpeg');
-        var imageMessage = new ROSLIB.Message({
-            format : 'jpeg',
-            data : data.replace('data:image/jpeg;base64,', '')
-        });
-        console.log(imageMessage);
-    }
+      const elapsed = (now - startTime) / 1000.0;
+      const fps = (++paintCount / elapsed).toFixed(3);
+      fpsInfo.innerText = !isFinite(fps) ? 0 : fps;
+      metadataInfo.innerText = JSON.stringify(metadata, null, 2);
+      var data = canvas.toDataURL('image/jpeg');
+      var imageMessage = new ROSLIB.Message({
+        format : 'jpeg',
+        data : data.replace('data:image/jpeg;base64,', '')
+      });
+      console.log(imageMessage)
+      video.requestVideoFrameCallback(updateCanvas);
+    };  
+    // let screenshot = function () {
+    //     console.log("bla")
+    //     ctx.drawImage(video, 0, 0, width, height);
+    //     var data = canvas.toDataURL('image/jpeg');
+    //     var imageMessage = new ROSLIB.Message({
+    //         format : 'jpeg',
+    //         data : data.replace('data:image/jpeg;base64,', '')
+    //     });
+    //     console.log(imageMessage);
+    //     video.requestVideoFrameCallback(screenshot);
+    // }
+    video.crossorigin ="anonymous"
     video.src =
     "https://cdn.glitch.com/c162fc32-0a96-4954-83c2-90d4cdb149fc%2FBig_Buck_Bunny_360_10s_20MB.mp4?v=1587545460302";
-    // video.requestVideoFrameCallback(updateCanvas);
+    video.requestVideoFrameCallback(updateCanvas);
   
 
     ss.addEventListener('click', () => screenshot);
