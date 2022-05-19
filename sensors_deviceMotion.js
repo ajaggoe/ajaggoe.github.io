b4b11ccad25e48235a96affacfd06b84bacb5d6a
Anish Jaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-05-19 10:21:14 +0200

    reformat

--- sensors_deviceMotion.js
+++ sensors_deviceMotion.js
@@ -44,35 +44,40 @@ if(requestPermission()){
 else{
     console.error("idk bro why you no work")
 }
-var ish = document.createElement("input")
-ish.type = "number"
-ish.value = 2
-console.log(parseInt(ish.value))
+
+function requestPermissionIOS(event) {
+  this.document.getElementById("device-info").innerHTML += `<br>AND WORKS IF IOS`
+  if(typeof(event.requestPermission) === "function"){
+    this.document.getElementById("device-info").innerHTML += `<br>AND REQUEST PERMISSION`
+    event.requestPermission().then((reponse) => {
+      if (reponse === 'granted') {
+        return true;
+      }
+    })
+  }
+  return false;
+}
 
 let iOS = !window.MSStream && /iPad|iPhone|iPod|Macintosh/.test(navigator.userAgent)
-console.log(iOS ? "blyat yes" : "blyat no")
-console.log(`number of touch is ${navigator.maxTouchPoints}`)
-console.log(typeof DeviceMotionEvent.requestPermission === 'function')
-this.document.getElementById("device-info").innerHTML = ` and blyat ${navigator.maxTouchPoints}`
 if(iOS){
-  this.document.getElementById("device-info").innerHTML += `<br>This is an iOS device, specifically: ${navigator.userAgent}`
+  this.document.getElementById("device-info").innerHTML += `<br>This is an iOS device`
   
   let permbutton = this.document.createElement("button")
   permbutton.setAttribute('id', 'perm');
   permbutton.innerHTML = "requestPermission"
-
+  let permission = false;
   permbutton.addEventListener('click', () => {
-  this.document.getElementById("device-info").innerHTML += `<br>BUTTON SHOULD WORK`
-    if(typeof(DeviceOrientationEvent.requestPermission) === "function"){
-      this.document.getElementById("device-info").innerHTML += `<br>AND REQUEST PERMISSION`
-      DeviceOrientationEvent.requestPermission().then((reponse) => {
-        if (reponse === 'granted') {
-          return true;
-        }
-      })
-    }
+    permission = !requestPermissionIOS(DeviceMotionEvent);
   });
-  
+
+  if(permission)
+  window.addEventListener('devicemotion', function(event) {
+    console.log(event.acceleration.x + ' m/s2');
+    if(event.acceleration.x){
+      this.document.getElementById("acc-info").innerHTML = `x: ${event.acceleration.x.toFixed(2)} <br>y: ${event.acceleration.y.toFixed(2)}`
+      this.document.getElementById("gyro").style.left = event.x * 3 + 20
+    }
+});
   this.document.body.appendChild(permbutton);
 } else {
   this.document.getElementById("device-info").innerHTML = `This is an not an iOS device, specifically: ${navigator.userAgent}`
